3d71316fe256ea877c6dffedd3769c0078fd0c88
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-06-29 22:45:56 +0530

    Update dbconnection.php

--- Curfew e-Pass Management System Project/cpms/admin/includes/dbconnection.php
+++ Curfew e-Pass Management System Project/cpms/admin/includes/dbconnection.php
@@ -2,7 +2,7 @@
 // DB credentials.
 define('DB_HOST','localhost');
 define('DB_USER','admin');
-define('DB_PASS','mahesh');
+define('DB_PASS','maheshkumar');
 define('DB_NAME','cpms');
 // Establish database connection.
 try
